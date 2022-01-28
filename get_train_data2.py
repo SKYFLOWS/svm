234cad5c0d3f11041e867e76894963de0ded6d5e
SKYFLOWS <92369699+SKYFLOWS@users.noreply.github.com>
2022-01-28 10:25:24 +0800

    Add files via upload

---
+++ get_train_data2.py
@@ -0,0 +1,19 @@
+import csv
+
+def transalte_label(filename):
+    #out = open('first.kddcup.data.corrected.csv', 'a', newline="")
+    out = open('first.kddcup.data_10_percent.corrected.csv', 'a', newline="")
+    csv_write = csv.writer(out, dialect="excel")
+    with open(filename, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            line = line.strip('\n')
+            line = line.split(',')
+            data = []
+            data.extend(line[22:42])
+            # print(data)
+            csv_write.writerow(data)
+
+if __name__ == "__main__":
+    #transalte_label('dos.kddcup.data.corrected.csv')
+    transalte_label('dos.kddcup.data_10_percent.corrected.csv')
